24cb4fd7dde6a12caf48618c0ba48cd887d06800
Maksym Shvets <maxim27061993@outlook.com>
2022-10-09 16:57:14 +0200

    task 01, 02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const ulArr = galleryItems.map((item) => {
+    const divRef = document.createElement("div");
+    divRef.classList.add("gallery__item");
+
+
+    const aRef = document.createElement("a");
+    aRef.setAttribute("href", `${item.original}`);
+    aRef.classList.add("gallery__link");
+
+    const imgRef = document.createElement("img");
+    imgRef.setAttribute("src", `${item.preview}`);
+    imgRef.setAttribute("data-source", `${item.original}`);
+    imgRef.setAttribute("alt", `${item.description}`);
+    imgRef.classList.add("gallery__image");
+
+    aRef.append(imgRef);
+    divRef.append(aRef);
+    return divRef;
+});
+galleryRef.append(...ulArr);
+
+
+function onGallery(event) {
+    event.preventDefault();
+
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    </div>
+`);
+
+    instance.show();
+    event.addEventListener("keydown", (event) => {
+        if (event.code === "Escape") {
+            instance.close();
+        }
+    });
+}
+galleryRef.addEventListener("click", onGallery);
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
 
+const ulArr = galleryItems.map((item) => {
+    const aRef = document.createElement("a");
+    aRef.setAttribute("href", `${item.original}`);
+    aRef.classList.add("gallery__link");
+
+    const imgRef = document.createElement("img");
+    imgRef.setAttribute("src", `${item.preview}`);
+    imgRef.setAttribute("data-source", `${item.original}`);
+    imgRef.setAttribute("alt", `${item.description}`);
+    imgRef.classList.add("gallery__image");
+
+    aRef.append(imgRef);
+    return aRef;
+});
+
+galleryRef.append(...ulArr);
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
 console.log(galleryItems);
